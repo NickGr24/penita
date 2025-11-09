9ac906f74ec76bc2d1ffb4675d3c61696d5caa4e
root <root@penitadreptului.md>
2025-11-09 11:21:51 +0000

    saving

--- penita/settings.py
+++ penita/settings.py
@@ -74,9 +74,12 @@ WSGI_APPLICATION = 'penita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'penita',
+        'USER': 'nikita',
+        'PASSWORD': 'nikita1717',
+        'HOST': 'localhost',
+        'PORT': '5432',    }
 }
 
 
